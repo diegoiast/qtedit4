30bb597c4aa278344956be2b83e9892832dd945b
Diego Iastrubni <diegoiast@gmail.com>
2008-01-09 13:31:54 +0000

    bad trunk! bad! go!

--- src/qelib/kate-qt/kateqtglobal.h
+++
@@ -1,9 +0,0 @@
-#ifndef __KATE_QT_GLOBAL_H__
-#define __KATE_QT_GLOBAL_H__
-
-#include <QString>
-#include <QMap>
-
-typedef QMap<QString,QString> QStringMap;
-
-#endif //__KATE_QT_GLOBAL_H__
--- src/qelib/qtsourceview/debug_info.h
+++
@@ -1,5 +0,0 @@
-// #define __DEBUG_NO_ITEM_FOUND
-//  #define __DEBUG_LANGS_MIMES__
-// #define __DEBUG_HIGHLIGHT__
-// #define __DEBUG_ADD_MAPPING__
-// #define __DEBUG_FOUND_LANG_DEF__
