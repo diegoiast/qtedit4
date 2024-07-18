501830499821a1cf23b416b848e6ce329abe5aba
Diego Iastrubni <diegoiast@gmail.com>
2024-07-18 17:53:46 +0300

    take 23

--- cmake/icons-breeze.cmake
+++ cmake/icons-breeze.cmake
@@ -5,8 +5,6 @@ function(download_breeze_icons VERSION)
     set(ZIP_FILE "${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}.zip")
     set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}")
     set(breeze_icons_install_dir "${CMAKE_BINARY_DIR}/share/icons/breeze")
-    file(TO_NATIVE_PATH "EXTRACT_DIR_NATIVE" ${EXTRACT_DIR})
-    file(TO_NATIVE_PATH "breeze_icons_install_dir_NATIVE" ${breeze_icons_install_dir})
 
     
     file(DOWNLOAD "${URL}" "${ZIP_FILE}" SHOW_PROGRESS INACTIVITY_TIMEOUT 10 STATUS download_result)
@@ -20,19 +18,19 @@ function(download_breeze_icons VERSION)
     message(" *** Extracting ${ZIP_FILE} into ${CMAKE_BINARY_DIR}")
     file(ARCHIVE_EXTRACT INPUT "${ZIP_FILE}" DESTINATION "${CMAKE_BINARY_DIR}")
 
-    message(" *** dir /w ${EXTRACT_DIR_NATIVE}")
+    message(" *** dir /w ${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}")
     execute_process(
-        COMMAND cmd /c "dir /w ${EXTRACT_DIR_NATIVE}"
+        COMMAND cmd /c "dir /w ${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}"
     )
 
-    message(" *** Copying  ${EXTRACT_DIR_NATIVE}/icons => ${breeze_icons_install_dir_NATIVE}" )
+    message(" *** Copying  ${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}/icons => ${CMAKE_BINARY_DIR}/share/icons/breeze")
     execute_process(
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${breeze_icons_install_dir_NATIVE}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR_NATIVE}/icons ${breeze_icons_install_dir}
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/share/icons/breeze"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory "${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}/icons" "${CMAKE_BINARY_DIR}/share/icons/breeze"
         RESULT_VARIABLE copy_result
     )
     if(copy_result)
-        message(FATAL_ERROR "Error copying directory from \"${EXTRACT_DIR_NATIVE}/icons\" to \"${breeze_icons_install_dir_NATIVE}\".")
+        message(FATAL_ERROR "Error copying ${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}/icons => ${CMAKE_BINARY_DIR}/share/icons/breeze")
     endif()
     
     set(${breeze_icons_install_dir_NATIVE} PARENT_SCOPE)
