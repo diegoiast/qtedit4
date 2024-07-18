0c353dea9d579f6f7502c74b9c8aaa1ba8c8d99a
Diego Iastrubni <diegoiast@gmail.com>
2024-07-18 08:16:24 +0300

    take 5

--- cmake/icons-breeze.cmake
+++ cmake/icons-breeze.cmake
@@ -13,12 +13,24 @@ function(download_breeze_icons VERSION)
         DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
         SOURCE_DIR ${EXTRACT_DIR}
         CONFIGURE_COMMAND
-            ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR}/icons ${CMAKE_BINARY_DIR}/share/icons/
         BUILD_COMMAND ""
         INSTALL_COMMAND ""
         LOG_DOWNLOAD ON
         DOWNLOAD_EXTRACT_TIMESTAMP ON
     )
+    # ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR}/icons ${CMAKE_BINARY_DIR}/share/icons/
 
-    set(${breeze_icons_install_dir} PARENT_SCOPE)
+    
+#   # Use 7-Zip to extract the archive
+#   add_custom_command(
+#     OUTPUT ${EXTRACT_DIR}
+#     COMMAND ${CMAKE_COMMAND} -E make_directory ${EXTRACT_DIR}
+#     COMMAND "7z.exe" "x" "${ZIP_FILE}" "-o${EXTRACT_DIR}" "-aoa"
+#     DEPENDS ${ZIP_FILE}
+#     COMMENT "Extracting breeze-icons archive"
+#     ERROR_VARIABLE extract_error
+#     RESULT_VARIABLE extract_result
+#   )
+   
+  set(${breeze_icons_install_dir} PARENT_SCOPE)
 endfunction()
