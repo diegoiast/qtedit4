700483029cc8bbf1d4e04382a687d43c3923b2be
Diego Iastrubni <diegoiast@gmail.com>
2024-07-18 17:44:50 +0300

    take 21

--- cmake/icons-breeze.cmake
+++ cmake/icons-breeze.cmake
@@ -5,6 +5,8 @@ function(download_breeze_icons VERSION)
     set(ZIP_FILE "${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}.zip")
     set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}")
     set(breeze_icons_install_dir "${CMAKE_BINARY_DIR}/share/icons/breeze")
+    file(TO_NATIVE_PATH "${EXTRACT_DIR_NATIVE}" ${EXTRACT_DIR})
+    file(TO_NATIVE_PATH "${breeze_icons_install_dir_NATIVE}" ${breeze_icons_install_dir})
 
     
     file(DOWNLOAD "${URL}" "${ZIP_FILE}" SHOW_PROGRESS INACTIVITY_TIMEOUT 10 STATUS download_result)
@@ -18,21 +20,20 @@ function(download_breeze_icons VERSION)
     message(" *** Extracting ${ZIP_FILE} into ${CMAKE_BINARY_DIR}")
     file(ARCHIVE_EXTRACT INPUT "${ZIP_FILE}" DESTINATION "${CMAKE_BINARY_DIR}")
 
-    file(TO_NATIVE_PATH "${path_with_forward_slashes}" ${EXTRACT_DIR})
-    message(" *** dir /w ${path_with_forward_slashes}")
+    message(" *** dir /w ${EXTRACT_DIR_NATIVE}")
     execute_process(
-        COMMAND cmd /c "dir /w ${path_with_forward_slashes}"
+        COMMAND cmd /c "dir /w ${EXTRACT_DIR_NATIVE}"
     )
 
-    message(" *** Copying  ${EXTRACT_DIR}/icons => ${breeze_icons_install_dir}" )
+    message(" *** Copying  ${EXTRACT_DIR_NATIVE}/icons => ${breeze_icons_install_dir_NATIVE}" )
     execute_process(
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${breeze_icons_install_dir}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR}/icons ${breeze_icons_install_dir}
+        COMMAND ${CMAKE_COMMAND} -E make_directory ${breeze_icons_install_dir_NATIVE}
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR_NATIVE}/icons ${breeze_icons_install_dir}
         RESULT_VARIABLE copy_result
     )
     if(copy_result)
-        message(FATAL_ERROR "Error copying directory from \"${EXTRACT_DIR}/icons\" to \"${breeze_icons_install_dir}\".")
+        message(FATAL_ERROR "Error copying directory from \"${EXTRACT_DIR_NATIVE}/icons\" to \"${breeze_icons_install_dir_NATIVE}\".")
     endif()
     
-    set(${breeze_icons_install_dir} PARENT_SCOPE)
+    set(${breeze_icons_install_dir_NATIVE} PARENT_SCOPE)
 endfunction()
