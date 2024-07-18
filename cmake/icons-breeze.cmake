f1a77221caf9cee0157f78f407c170dda652f8d5
Diego Iastrubni <diegoiast@gmail.com>
2024-07-18 14:19:49 +0300

    take 15

--- cmake/icons-breeze.cmake
+++ cmake/icons-breeze.cmake
@@ -6,22 +6,38 @@ function(download_breeze_icons VERSION)
     set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/breeze-icons-${VERSION}")
 
     set(breeze_icons_install_dir "${CMAKE_BINARY_DIR}/share/icons/breeze")
+
     ExternalProject_Add(
         breeze_icons_project
         URL ${URL}
         DOWNLOAD_NO_PROGRESS False
         DOWNLOAD_DIR ${CMAKE_BINARY_DIR}
         SOURCE_DIR ${EXTRACT_DIR}
-        CONFIGURE_COMMAND
-            ${CMAKE_COMMAND} -E list_directory ${EXTRACT_DIR}
-            ${CMAKE_COMMAND} -E list_directory ${EXTRACT_DIR}/icons
-            ${CMAKE_COMMAND} -E list_directory ${breeze_icons_install_dir}
-            ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR}/icons ${breeze_icons_install_dir}
-        BUILD_COMMAND ""
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ${CMAKE_COMMAND} -E make_directory ${breeze_icons_install_dir}
+                      COMMAND ${CMAKE_COMMAND} -E copy_directory ${EXTRACT_DIR}/icons ${breeze_icons_install_dir}
         INSTALL_COMMAND ""
         LOG_DOWNLOAD ON
         DOWNLOAD_EXTRACT_TIMESTAMP ON
     )
 
-    set(${breeze_icons_install_dir} PARENT_SCOPE)
+    # Print directory structure of EXTRACT_DIR before copying
+    ExternalProject_Add_Step(
+        breeze_icons_project print_extract_dir_structure
+        COMMAND ${CMAKE_COMMAND} -E echo "Contents of ${EXTRACT_DIR}:"
+        COMMAND ${CMAKE_COMMAND} -E list_directory ${EXTRACT_DIR}
+        ALWAYS 1
+        DEPENDEES download
+    )
+
+    # Print directory structure of breeze_icons_install_dir before copying
+    ExternalProject_Add_Step(
+        breeze_icons_project print_install_dir_structure
+        COMMAND ${CMAKE_COMMAND} -E echo "Contents of ${breeze_icons_install_dir}:"
+        COMMAND ${CMAKE_COMMAND} -E list_directory ${breeze_icons_install_dir}
+        ALWAYS 1
+        DEPENDEES print_extract_dir_structure
+    )
+
+    set(${breeze_icons_install_dir} ${breeze_icons_install_dir} PARENT_SCOPE)
 endfunction()
