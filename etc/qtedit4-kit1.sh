ccec8b58b1e6c3534f8cce344482bce167a841a5
Diego Iastrubni <diegoiast@gmail.com>
2024-08-04 22:12:16 +0300

    Meta:kit merge
    Crap, this worked out of the box on Windows. Lets also commit the bat
    version of the MSVC kit.

--- etc/qtedit4-kit1.sh
+++ etc/qtedit4-kit1.sh
@@ -1,8 +1,21 @@
 #! /bin/sh
 
-# @@ name = Qt 6.7.1 in ~/qt
-# @@ author = qtedit4
+# This is a kit definition for qtedit4. All your tasks
+# will run trought this file.
 
+# available enritonment variables:
+# %source_directory% - where the source being executed is saved
+# %build_directory%  - where code should be compile into
+# %run_directory%    - where this taks should be run, as defined in
+#                      the task's definition in the JSON file
+# %task%             - the actual code to be run
+
+# The following meta variables are for qtedit4. Note the prefix:
+# @@ name = Qt 6.7.1 in ~/qt
+# @@ author = auto generated - by qtedit4
+
+# from this point on - echo is on. Every command will be displayed
+# in the build output.
 set -x
 set -e
 
