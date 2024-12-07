24a8028eb8520c580f3d5b5a6d3e27326438b728
Diego Iastrubni <diegoiast@gmail.com>
2024-12-07 22:16:28 +0200

    TextOperationsWidget: mark text when choosing line
    When the goto line widget is displayed, text is marked. Meaning you can
    just change the line number.

--- src/widgets/textoperationswidget.cpp
+++ src/widgets/textoperationswidget.cpp
@@ -495,6 +495,7 @@ void TextOperationsWidget::showGotoLine() {
     }
 
     showBottomWidget(gotoLineWidget);
+    gotoLineFormUi->numberSpinBox->selectAll();
     gotoLineWidget->setFocus();
     gotoLineFormUi->numberSpinBox->setFocus();
 }
