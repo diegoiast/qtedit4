diegoiast/qtedit4

commit f5bb08f7537d3e45fdad30eda8b0fb67b2d6b863

qmdiEditor: modified title name (#49)
When the editor is modified, the document name will contain  "*". This
is also propagated to the window title, not only the tab's title.

closes #49

src/widgets/qmdieditor.cpp
@@ -459,6 +459,7 @@ void qmdiEditor::setupActions() {
     connect(textEditor, &QPlainTextEdit::copyAvailable, actionCut, &QAction::setEnabled);
     connect(textEditor, &QPlainTextEdit::undoAvailable, actionUndo, &QAction::setEnabled);
     connect(textEditor, &QPlainTextEdit::redoAvailable, actionRedo, &QAction::setEnabled);
+    connect(textEditor, &QPlainTextEdit::textChanged, this, &qmdiEditor::updateClientName);
 
     connect(actionCopyFileName, &QAction::triggered, actionCopyFileName, [this]() {
         auto c = QApplication::clipboard();
@@ -623,6 +624,27 @@ void qmdiEditor::hideTimer_timeout() {
     }
 }
 
+void qmdiEditor::updateClientName() {
+    auto MODIFIED_TEXT = " *";
+    if (textEditor->document()->isModified()) {
+        if (!mdiClientName.contains(MODIFIED_TEXT)) {
+            mdiClientName = getShortFileName() + MODIFIED_TEXT;
+            auto w = dynamic_cast<QTabWidget *>(this->mdiServer);
+            auto i = w->indexOf(this);
+            w->setTabText(i, mdiClientName);
+            mdiServer->mdiSelected(this, i);
+        }
+    } else {
+        if (mdiClientName.contains(MODIFIED_TEXT)) {
+            mdiClientName = getShortFileName();
+            auto w = dynamic_cast<QTabWidget *>(this->mdiServer);
+            auto i = w->indexOf(this);
+            w->setTabText(i, mdiClientName);
+            mdiServer->mdiSelected(this, i);
+        }
+    }
+}
+
 void qmdiEditor::goTo(int x, int y) {
     if (documentHasBeenLoaded) {
         textEditor->goTo(x, y);
@@ -719,6 +741,7 @@ bool qmdiEditor::loadFile(const QString &newFileName) {
     }
 
     documentHasBeenLoaded = false;
+    updateClientName();
     return true;
 }
 
@@ -771,6 +794,7 @@ bool qmdiEditor::saveFile(const QString &newFileName) {
     }
     file.close();
     textEditor->document()->setModified(false);
+    updateClientName();
 
     QApplication::restoreOverrideCursor();
 
@@ -1000,6 +1024,12 @@ void qmdiEditor::loadContent() {
     this->originalLineEnding = getLineEnding(file);
     auto textStream = QTextStream(&file);
     textStream.seek(0);
+
+    // why blocking signals?
+    // when loading, (setPlainText()) a signal is emited,  which triggers the system to believe
+    // that the content has been modified. just don't do this. It will also save some time
+    // on loading, since really, signals emited a this stage are not meaningful.
+    textEditor->blockSignals(true);
     textEditor->setPlainText(textStream.readAll());
     textEditor->goTo(requestedPosition.x(), requestedPosition.y());
 
@@ -1019,8 +1049,10 @@ void qmdiEditor::loadContent() {
     updateFileDetails();
     setModificationsLookupEnabled(modificationsEnabledState);
     textEditor->removeModifications();
+    textEditor->blockSignals(false);
     QApplication::restoreOverrideCursor();
     documentHasBeenLoaded = true;
+    updateClientName();
 
     if (auto tab = dynamic_cast<qmdiTabWidget *>(mdiServer)) {
         emit tab->newClientAdded(this);

src/widgets/qmdieditor.h
@@ -103,6 +103,7 @@ class qmdiEditor : public QWidget, public qmdiClient {
 
     void fileMessage_clicked(const QString &s);
     void hideTimer_timeout();
+    void updateClientName();
 
   public:
     QString originalLineEnding = "\n";

commit b2b94cc734b068de759079b6a7362932c09aa984

qmdiEditor: Fix preview
Fixed a regression caused by https://github.com/diegoiast/qtedit4/commit/f5bb08f7537d3e45fdad30eda8b0fb67b2d6b863

Problem is that we look at the display text, which might have at the end
a marker. Fixed by looking for extention in the full filename.

refs #49

src/widgets/qmdieditor.cpp
@@ -367,7 +367,6 @@ void qmdiEditor::setupActions() {
 
     // FIXME - the new syntax for connecting a signal/slot crashes the app, using the old one works
     // connect(textEditor, &QPlainTextEdit::textChanged, this, &qmdiEditor::updatePreview);
-    connect(textEditor, SIGNAL(textChanged()), this, SLOT(updatePreview()));
 
     actionSave = new QAction(QIcon::fromTheme("document-save"), tr("&Save"), this);
     actionSaveAs = new QAction(QIcon::fromTheme("document-save-as"), tr("Save &as..."), this);
@@ -569,16 +568,12 @@ void qmdiEditor::setHistoryModel(SharedHistoryModel *model) {
 }
 
 bool qmdiEditor::isMarkDownDocument() const {
-    return mdiClientName.endsWith(".md", Qt::CaseInsensitive);
+    return fileName.endsWith(".md", Qt::CaseInsensitive);
 }
 
-bool qmdiEditor::isXPMDocument() const {
-    return mdiClientName.endsWith(".xpm", Qt::CaseInsensitive);
-}
+bool qmdiEditor::isXPMDocument() const { return fileName.endsWith(".xpm", Qt::CaseInsensitive); }
 
-bool qmdiEditor::isSVGDocument() const {
-    return mdiClientName.endsWith(".svg", Qt::CaseInsensitive);
-}
+bool qmdiEditor::isSVGDocument() const { return fileName.endsWith(".svg", Qt::CaseInsensitive); }
 
 bool qmdiEditor::isXMLDocument() const {
     QRegularExpression regex(R"(.*xml.*\.xml$)", QRegularExpression::CaseInsensitiveOption);
@@ -625,6 +620,8 @@ void qmdiEditor::hideTimer_timeout() {
 }
 
 void qmdiEditor::updateClientName() {
+    updatePreview();
+
     auto MODIFIED_TEXT = " *";
     if (textEditor->document()->isModified()) {
         if (!mdiClientName.contains(MODIFIED_TEXT)) {