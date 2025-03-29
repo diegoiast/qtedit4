diegoiast/qtedit4

commit 6e1fa3a0acb3005ed56e6f1d4d10474dbad91879

qmdiEditor, HexViewrPlugin: notify loading of a text file
In preparation for #52, lets announce to the plugins "hey - a new file
has been loaded". When the project manager will decide "if this project
has not been loaded, lets ask the user".

The hex viewer plugin was eating those requests, so it needed fixing.

This is a new way of using the plugin system, which has not been tested
yet, but it was the intention when developing it.

src/plugins/hexviewer/hexviewer_plg.cpp
@@ -149,6 +149,11 @@ QStringList HexViewrPlugin::myExtensions() {
 int HexViewrPlugin::canOpenFile(const QString &fileName) {
     static const QStringList extensions = {".bin", ".img", "blob", ".so",   ".AppImage",
                                            ".a",   ".exe", ".dll", ".dlib", ".pdf"};
+
+    auto uri = QUrl(fileName);
+    if (uri.scheme().isEmpty() || uri.scheme() != "file") {
+        return -1;
+    }
     for (const QString &ext : extensions) {
         if (fileName.endsWith(ext, Qt::CaseInsensitive)) {
             return 6;

src/widgets/qmdieditor.cpp
@@ -1051,7 +1051,11 @@ void qmdiEditor::loadContent() {
     documentHasBeenLoaded = true;
     updateClientName();
 
+    // TODO - we should remove dependency on qmdiTabWidget
     if (auto tab = dynamic_cast<qmdiTabWidget *>(mdiServer)) {
         emit tab->newClientAdded(this);
     }
+
+    auto pluginManager = dynamic_cast<PluginManager *>(mdiServer->mdiHost);
+    pluginManager->openFile("loaded:" + fileName);
 }

commit f0106359dfbf164f8b1e92a18f39feeaf09bd88a

HexViewrPlugin: fix loading
Loading of binary files was broken in
6e1fa3a0acb3005ed56e6f1d4d10474dbad91879.

Fixed names of this plugin and the ImageViewrPlugin.

src/plugins/hexviewer/hexviewer_plg.cpp
@@ -131,7 +131,7 @@ class qmdiHexViewer : public QHexView, public qmdiClient {
 };
 
 HexViewrPlugin::HexViewrPlugin() {
-    name = tr("Image viewer plugin - based on QutePart");
+    name = tr("Hex viewer plugin");
     author = tr("Diego Iastrubni <diegoiast@gmail.com>");
     iVersion = 0;
     sVersion = "0.0.1";
@@ -153,7 +153,8 @@ int HexViewrPlugin::canOpenFile(const QString &fileName) {
                                            ".a",   ".exe", ".dll", ".dlib", ".pdf"};
 
     auto uri = QUrl(fileName);
-    if (uri.scheme().isEmpty() || uri.scheme() != "file") {
+    auto scheme = uri.scheme();
+    if (!scheme.isEmpty() && scheme != "file") {
         return -1;
     }
     for (const QString &ext : extensions) {

src/plugins/imageviewer/imageviewer_plg.cpp
@@ -70,7 +70,7 @@ class qmdiImageViewer : public pal::ImageViewer, public qmdiClient {
 };
 
 ImageViewrPlugin::ImageViewrPlugin() {
-    name = tr("Image viewer plugin - based on QutePart");
+    name = tr("Image viewer plugin");
     author = tr("Diego Iastrubni <diegoiast@gmail.com>");
     iVersion = 0;
     sVersion = "0.0.1";