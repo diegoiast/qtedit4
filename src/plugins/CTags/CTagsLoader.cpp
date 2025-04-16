28232e0ab10be49837879b84fbd678a4da969532
Diego Iastrubni <diegoiast@gmail.com>
2025-04-16 16:21:28 +0300

    Fix compilation on cl

--- src/plugins/CTags/CTagsLoader.cpp
+++ src/plugins/CTags/CTagsLoader.cpp
@@ -4,20 +4,16 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <regex>
 #include <sstream>
 #include <string>
 #include <thread>
 
 #if defined(_WIN32) || defined(_WIN64)
-
-// We don't mind using open(), peopen() and friends
-#pragma warning(suppress : 4996)
-#include <cstdio> // For popen and pclose
 #include <io.h>
-
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#define popen _popen
+#define pclose _pclose
 #else
 #include <cstdio> // For popen and pclose
 #include <sys/types.h>
