9c510b73e78345a090c14b4dea23d560f1b96c6a
Diego Iastrubni <diegoiast@gmail.com>
2025-04-21 22:22:01 +0300

    fix code to compile

--- src/plugins/ProjectManager/ProjectManagerPlg.cpp
+++ src/plugins/ProjectManager/ProjectManagerPlg.cpp
@@ -366,7 +366,6 @@ void ProjectManagerPlugin::on_client_merged(qmdiHost *host) {
     outputPanel->commandOuput->setOpenExternalLinks(false);
     outputPanel->commandOuput->setOpenLinks(false);
 
-<<<<<<< HEAD
     auto font = QFontDatabase::systemFont(QFontDatabase::FixedFont);
     outputPanel->commandOuput->setFont(font);
     outputPanel->commandOuput->setAcceptRichText(true);
@@ -407,8 +406,6 @@ void ProjectManagerPlugin::on_client_merged(qmdiHost *host) {
                 getManager()->openFile(fileName, row, col);
             });
 
-=======
->>>>>>> 02a3365 (Rebranding - nicer UI)
     connect(outputPanel->clearOutput, &QAbstractButton::clicked, this,
             [this]() { this->outputPanel->commandOuput->clear(); });
     connect(outputPanel->copyOutput, &QAbstractButton::clicked, this, [this]() {
