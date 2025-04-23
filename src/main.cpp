fa933418e64765d141939d31702f76dc4a925a48
Diego Iastrubni <diegoiast@gmail.com>
2025-04-23 19:07:44 +0300

    Version 0.0.9-beta2

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName("qtedit4");
-    QCoreApplication::setApplicationVersion("0.0.9-beta1");
+    QCoreApplication::setApplicationVersion("0.0.9-beta2");
 
 #if defined(WIN32)
     // default style on windows is ugly and unusable.
