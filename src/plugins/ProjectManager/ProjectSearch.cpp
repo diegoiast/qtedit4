74632a8d6837ec1562ea8a09559b6ebb8998d8b6
Diego Iastrubni <diegoiast@gmail.com>
2025-05-08 22:33:34 +0300

    ProjectSearch: remove leading dir character (#87)
    When searching, we search in relative to the project root (or search
    path). Thus, all paths are relative.
    
    closes #87

--- src/plugins/ProjectManager/ProjectSearch.cpp
+++ src/plugins/ProjectManager/ProjectSearch.cpp
@@ -233,6 +233,9 @@ void ProjectSearch::searchButton_clicked() {
             }
             auto shortFileName = fullFileName.mid(trimCount);
             auto searchInBinaries = ui->searchInBinaryFiles->isChecked();
+            if (shortFileName.startsWith('/') || shortFileName.startsWith('\\') ) {
+                shortFileName.remove(0, 1);
+            }
 
             if (!FilenameMatches(fullFileName, allowList, denyList)) {
                 continue;
