f9c6e0166b0ad7608b9ad13d1c72f4931ad4feb3
Diego Iastrubni <diegoiast@gmail.com>
2025-05-30 15:38:15 +0300

    Version 0.0.10-rc1

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
     QCoreApplication::setApplicationName("qtedit4");
-    QCoreApplication::setApplicationVersion("0.0.10-alpha1");
+    QCoreApplication::setApplicationVersion("0.0.10-rc1");
 
 #if defined(WIN32)
     // default style on windows is ugly and unusable.
