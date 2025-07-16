69b7835eecdac6687b211f2d5529c114542f7fe5
Diego Iastrubni <diegoiast@gmail.com>
2025-07-16 14:30:13 +0300

    build.sh fix building on Arch
    1) For some reason here, the Qt plugin on AppImage is very insisting of
       using `/usr/bin/qmake5` instead of the one in `QTDIR`. Lets force this.
    2) I tested changing the matrix config dir, and it fails. I missed another place
       where I needed to use a variable.

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ set -e
 QT_VERSION="6.9.1"
 APP_VERSION="0.0.11"
 
-
 NAME="qtedit4-qt${QT_VERSION}-v${APP_VERSION}-dev-x86_64"
-QTDIR="$HOME/qt/${QT_VERSION}/gcc_64/"
+QTDIR="$HOME/qt/${QT_VERSION}/gcc_64"
 export matrix_config_build_dir=ubuntu-gcc
 export PATH=$QTDIR/bin:$PATH
 export LD_LIBRARY_PATH=$QTDIR/lib:$LD_LIBRARY_PATH
 export OUTPUT=dist/$NAME.AppImage
+export QMAKE="${QTDIR}/bin/qmake"
 
 rm -fr "build/${matrix_config_build_dir}"
 rm -fr "dist"
@@ -26,7 +26,7 @@ cmake --install "build/${matrix_config_build_dir}" --prefix dist/${matrix_config
 
 rm -fr dist/qtedit4
 mkdir -p dist/qtedit4/
-ln -s  ../ubuntu-gcc/usr/ dist/qtedit4/$NAME
+ln -s  ../${matrix_config_build_dir}/usr/ dist/qtedit4/$NAME
 tar -cjhvf dist/$NAME.tar.bz2 -C dist/qtedit4 .
 
 # Qt plugin does not bundle the needed plugins, do this manually
@@ -39,3 +39,4 @@ wget -nc --no-verbose "https://github.com/linuxdeploy/linuxdeploy/releases/downl
 wget -nc --no-verbose "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage"
 chmod +x linuxdeploy*.AppImage
 ./linuxdeploy-x86_64.AppImage --appdir "dist/${matrix_config_build_dir}" --plugin qt --output appimage
+
