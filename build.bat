337cd10aab4b123b15dee53c446fd1b7e343dc9a
Diego Iastrubni <diegoiast@gmail.com>
2025-12-16 22:55:41 +0200

    Flatpack: initial support (#24)
    Added the needed parts to at least build locally the flatpack.
    Adding the `appdata.xml` was very beneficial, as it added more
    metadata to the AppImage.
    
    Offline support got fixed, as sub projects gained offline support, and
    also - we are no longer installing the icons on such scenarios.
    
    Not a full win, as I there is no automatic way to sync the subprojects
    in the cmake to the build yml, there is no support for build flavors,
    and the build is no upstream.
    
    But, its a good start.
    
    refs #24

--- build.bat
+++ build.bat
@@ -2,13 +2,14 @@
 
 SET matrix_config_build_dir=windows-msvc
 SET PATH=c:\Qt\6.10.1\msvc2022_64\bin\;c:\Program Files (x86)\Inno Setup 6\;%PATH%
+SET BUILD_TYPE=OFFICIAL
 
 RMDIR /s /q "build/%matrix_config_build_dir%"
 RMDIR /s /q dist
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-cmake . -B  "build/%matrix_config_build_dir%" -DCMAKE_BUILD_TYPE=Release -A x64
+cmake . -B  "build/%matrix_config_build_dir%" -DCMAKE_BUILD_TYPE=Release -A x64 -DBUILD_VERSION=%BUILD_TYPE%
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build   "build/%matrix_config_build_dir%" --parallel --verbose --config Release
 if %errorlevel% neq 0 exit /b %errorlevel%
