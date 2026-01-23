abad5699348fadcf803f29a6ff63ccff1548c341
Diego Iastrubni <diegoiast@gmail.com>
2026-01-23 11:27:48 +0200

    TerminalPlugin: start working on settings
    The config dialog has several options, still not enabled.
    
    The main work done on the preview: it needs new API from qmdilib, also
    updated Kodoterm to a newer sha1 from main.

--- src/plugins/Terminal/TerminalPlugin.cpp
+++ src/plugins/Terminal/TerminalPlugin.cpp
@@ -5,12 +5,15 @@
  * License MIT
  */
 
-#include <QSettings>
 #include <QDockWidget>
+#include <QFontDatabase>
 #include <QKeySequence>
+#include <QSettings>
 
-#include <plugins/Terminal/TerminalPlugin.hpp>
-#include "KodoTerm/KodoTerm.hpp"
+#include <KodoTerm/KodoTerm.hpp>
+#include <qmdidialogevents.hpp>
+
+#include "plugins/Terminal/TerminalPlugin.hpp"
 
 static QString systemCurrentShell() {
     QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
@@ -37,7 +40,7 @@ TerminalPlugin::TerminalPlugin() {
     sVersion = "0.0.1";
     autoEnabled = true;
     alwaysEnabled = false;
-    
+
     toggleTerminal = new QAction(tr("Toggle terminal"), this);
     toggleTerminal->setShortcut(QKeySequence("Ctrl+T"));
     connect(toggleTerminal, &QAction::triggered, this, [this]() {
@@ -50,8 +53,106 @@ TerminalPlugin::TerminalPlugin() {
             console->setFocus();
         }
     });
-    
+
     menus[tr("&File")]->addAction(toggleTerminal);
+
+    theme = TerminalTheme::defaultTheme();
+#ifdef Q_OS_WIN
+    auto pseudoPrompt = QString("C:\\> ver<br>Microsoft Windows [Version 10.0.19045.4170]");
+#else
+    auto pseudoPrompt =
+        QString("user@localhost:~$ uptime<br>12:34:56 up 10 days,  1:23,  2<br>users, "
+                " load average: 0.05, 0.01, 0.00");
+#endif
+
+    auto colorsSpan = QString("<br>");
+    for (auto i = 0; i < 16; i++) {
+        auto c1 = theme.palette[i].name();
+        auto c2 = theme.palette[16 - i].name();
+        auto c3 = QString("<span style='background-color: %1; color: %2'>x</span>").arg(c1, c1);
+        colorsSpan += c3;
+    }
+    auto monospacedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
+    monospacedFont.setFixedPitch(true);
+
+    config.pluginName = tr("Terminal");
+
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(tr("Display font"))
+                                     .setKey(Config::FontKey)
+                                     .setType(qmdiConfigItem::Font)
+                                     .setDefaultValue(monospacedFont)
+                                     .setValue(monospacedFont)
+                                     .build());
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(tr("Theme"))
+                                     .setDescription(tr("Which theme to use for the terminal"))
+                                     .setKey(Config::ThemeFileKey)
+                                     .setType(qmdiConfigItem::Label)
+                                     //.setDefaultValue("ctags-universal")
+                                     //.setPossibleValue(true)
+                                     .build());
+    config.configItems.push_back(
+        qmdiConfigItem::Builder()
+            .setDisplayName(tr("Choose theme"))
+            .setDescription(tr("Click to choose form internal themes avilable"))
+            .setKey(Config::ThemeFileChooseKey)
+            .setType(qmdiConfigItem::Button)
+            .build());
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(pseudoPrompt + colorsSpan)
+                                     .setKey(Config::PromptPreviewKey)
+                                     .setType(qmdiConfigItem::Label)
+                                     .build());
+
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(tr("Double click selects line"))
+                                     .setKey(Config::DoubleClickKey)
+                                     .setType(qmdiConfigItem::Bool)
+                                     .setDefaultValue(true)
+                                     .build());
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(tr("Tripple click selects whole line"))
+                                     .setKey(Config::TrippleClickClickKey)
+                                     .setType(qmdiConfigItem::Bool)
+                                     .setDefaultValue(true)
+                                     .build());
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(tr("Copy text on selection"))
+                                     .setKey(Config::CopyOnSelectKey)
+                                     .setType(qmdiConfigItem::Bool)
+                                     .setDefaultValue(true)
+                                     .build());
+    config.configItems.push_back(qmdiConfigItem::Builder()
+                                     .setDisplayName(tr("Paste on middle mouse click"))
+                                     .setKey(Config::PasteOnMiddleClickKey)
+                                     .setType(qmdiConfigItem::Bool)
+                                     .setDefaultValue(true)
+                                     .build());
+
+    connect(&qmdiDialogEvents::instance(), &qmdiDialogEvents::buttonClicked, this,
+            [this](qmdiConfigDialog *dialog, const QString &buttonKey) {
+                if (buttonKey == Config::ThemeFileKey) {
+                    // downloadCTags(dialog);
+                }
+            });
+    connect(&qmdiDialogEvents::instance(), &qmdiDialogEvents::widgetCreated, this,
+            [this, monospacedFont](auto dialog, auto const &item, auto label, auto widget) {
+                qDebug() << "Created widget" << item.key;
+                if (item.key == Config::PromptPreviewKey) {
+                    label->setFrameStyle(QFrame::Panel);
+                    label->setFont(monospacedFont);
+                    auto pal = label->palette();
+                    pal.setColor(QPalette::Window, this->theme.background);
+                    pal.setColor(QPalette::WindowText, this->theme.foreground);
+                    label->setAutoFillBackground(true);
+                    label->setPalette(pal);
+                }
+
+                Q_UNUSED(dialog);
+                Q_UNUSED(widget);
+                // Q_UNUSED(this);
+            });
 }
 
 TerminalPlugin::~TerminalPlugin() {
@@ -66,11 +167,9 @@ void TerminalPlugin::on_client_merged(qmdiHost *host) {
     console->start();
     terminalDock = manager->createNewPanel(Panels::South, "terminalPanel", tr("Terminal"), console);
 }
-    
-void TerminalPlugin::on_client_unmerged(qmdiHost *) {
-    delete terminalDock;
-}
-    
+
+void TerminalPlugin::on_client_unmerged(qmdiHost *) { delete terminalDock; }
+
 void TerminalPlugin::loadConfig(QSettings &settings) {
     // TODO
 }
