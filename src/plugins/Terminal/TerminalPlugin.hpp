f34fb55792b880d2efd228b14c682d5b12d25351
Diego Iastrubni <diegoiast@gmail.com>
2026-01-23 11:27:48 +0200

    TerminalPlugin: start working on settings
    The config dialog has several options, still not enabled.
    
    The main work done on the preview: it needs new API from qmdilib, also
    updated Kodoterm to a newer sha1 from main.

--- src/plugins/Terminal/TerminalPlugin.hpp
+++ src/plugins/Terminal/TerminalPlugin.hpp
@@ -8,24 +8,43 @@
 #pragma once
 
 #include "iplugin.h"
+#include "KodoTerm/KodoTermConfig.hpp"
+
 
 class QDockWidget;
 class QAction;
 class KodoTerm;
 
 class TerminalPlugin : public IPlugin {
+    struct Config {
+        CONFIG_DEFINE(Font, QString)
+        CONFIG_DEFINE(ThemeFile, QString)
+        CONFIG_DEFINE(ThemeFileChoose, QString)
+        CONFIG_DEFINE(PromptPreview, QString)
+        CONFIG_DEFINE(DoubleClick, bool)
+        CONFIG_DEFINE(TrippleClickClick, bool)
+        CONFIG_DEFINE(CopyOnSelect, bool)
+        CONFIG_DEFINE(PasteOnMiddleClick, bool)
+        qmdiPluginConfig *config;
+    };
+    Config &getConfig() {
+        static Config configObject{&this->config};
+        return configObject;
+    }
+
     Q_OBJECT
   public:
     TerminalPlugin();
     ~TerminalPlugin();
-    
+
     // IPlugin interface
     virtual void on_client_merged(qmdiHost *host) override;
     virtual void on_client_unmerged(qmdiHost *host) override;
     virtual void loadConfig(QSettings &settings) override;
-    
+
   private:
     QDockWidget *terminalDock = nullptr;
     QAction *toggleTerminal;
     KodoTerm *console = nullptr;
+    TerminalTheme theme;
 };
